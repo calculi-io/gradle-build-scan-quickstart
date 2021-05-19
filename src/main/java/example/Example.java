d0427a49c14bbea0cb96d004f35eb05a58ec6aec
Yoga Ganesha <yoga.ganesha@calculi.com>
2021-05-19 08:58:00 -0700

    add security issue for codeQL test

--- src/main/java/example/Example.java
+++ src/main/java/example/Example.java
@@ -2,9 +2,25 @@ package example;
 
 import com.google.common.base.Joiner;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+
 public class Example {
 
     public static String join(String... args) {
+        try {
+            InputStream is = new InputStream() {
+                @Override
+                public int read() throws IOException {
+                    return 0;
+                }
+            };
+            ObjectInputStream ois = new ObjectInputStream(is);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
         return Joiner.on(' ').join(args);
     }
 
